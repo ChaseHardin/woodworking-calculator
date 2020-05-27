98d0c19a7fc608f53659caec55761b2001fc69e8
Chase Hardin <chase.hardin@scribbletechnology.com>
2020-05-26 21:21:02 -0500

    Wrap app in bootstrap container

--- src/App.js
+++ src/App.js
@@ -1,28 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { CutDetailsForm } from './components/cut-details-form/cut-details-form';
+import Container from 'react-bootstrap/Container';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <CutDetailsForm />
-    </div>
+    <Container>
+      <div className="App">
+        <CutDetailsForm />
+      </div>
+    </Container>
   );
 }
 
