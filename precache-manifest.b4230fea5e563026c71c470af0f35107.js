91c3bd11a804bda36d84f42a626458ad9fcb2037
Chase Hardin <chase.hardin@scribbletechnology.com>
2020-05-26 21:41:02 -0500

    Updates

---
+++ precache-manifest.b4230fea5e563026c71c470af0f35107.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5bf464e5a819995f48aa70ee7b738bd",
+    "url": "/woodworking-calculator/index.html"
+  },
+  {
+    "revision": "ca15c13288db6b5a158a",
+    "url": "/woodworking-calculator/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "ebbe5d0ad916601e18bf",
+    "url": "/woodworking-calculator/static/css/main.f4bc1fdf.chunk.css"
+  },
+  {
+    "revision": "ca15c13288db6b5a158a",
+    "url": "/woodworking-calculator/static/js/2.9e4688d2.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/woodworking-calculator/static/js/2.9e4688d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebbe5d0ad916601e18bf",
+    "url": "/woodworking-calculator/static/js/main.7358749b.chunk.js"
+  },
+  {
+    "revision": "053ce2d5c6597497654a",
+    "url": "/woodworking-calculator/static/js/runtime-main.91aa8c58.js"
+  }
+]);
